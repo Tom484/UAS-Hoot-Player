337c02275120a0122699eec0d0fae18f8fce5397
Tomáš Kůrka <tom589927@gmail.com>
2021-12-18 13:29:01 +0100

    added dots animation

--- client/src/components/components/loading/Loading.jsx
+++ client/src/components/components/loading/Loading.jsx
@@ -1,17 +1,19 @@
 import React from "react"
 import BubbleBackground from "../bubbleBackground/BubbleBackground"
 import LineBackground from "../lineBackground/LineBackground"
+import LoadingCard from "../loadingCard/LoadingCard"
+import RollerAnimation from "../../animation/rollerAnimation/RollerAnimation"
 
 import "./loading.scss"
 
 const Loading = ({ label }) => {
   return (
     <div className="loading-component">
-      {/* <SquareAnimation /> */}
-      {/* <p className="loading-label">{label}</p> */}
       <LineBackground />
       <BubbleBackground />
-      <div className="loading-component-container">Lore</div>
+      <div className="loading-component-container">
+        <LoadingCard label={label} />
+      </div>
     </div>
   )
 }
--- client/src/components/components/loadingCard/LoadingCard.jsx
+++ client/src/components/components/loadingCard/LoadingCard.jsx
@@ -1,11 +1,26 @@
 import React from "react"
+import img from "../../../img/img-2.svg"
+import DotsAnimation from "../../animation/dotsAnimation/DotsAnimation"
 
 import "./loadingCard.scss"
 
-const LoadingCard = () => {
+const LoadingCard = ({ label }) => {
   return (
     <div className="loading-card">
-      <div className="loading-card-container"></div>
+      <div className="loading-card-container">
+        <div className="picture" style={{ backgroundImage: `url(${img})` }}></div>
+        <div className="content">
+          <div className="name">{label}</div>
+          <div className="question">Did you know?</div>
+          <div className="facts-container">
+            <div className="fact">
+              The world’s oldest wooden wheel has been around for more than 5,000 years.
+            </div>
+            <div className="fact">Sudan has more pyramids than any country in the world.</div>
+          </div>
+          <DotsAnimation />
+        </div>
+      </div>
     </div>
   )
 }
