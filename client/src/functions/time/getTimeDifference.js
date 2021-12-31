e24c57de70f3287a5eb06a0508c0e22a0af7bea2
Tomas Kurka <tom589927@gmail.com>
2021-12-31 19:11:32 +0100

    Add fix get time bug

--- client/src/functions/time/getTimeDifference.js
+++ client/src/functions/time/getTimeDifference.js
@@ -3,7 +3,9 @@ export async function getTimeDifference() {
   try {
     const startTime = new Date()
     let diff
-    const response = await fetch("http://worldtimeapi.org/api/timezone/Europe/Prague")
+    const response = await fetch(
+      "https://www.timeapi.io/api/Time/current/zone?timeZone=Europe/Prague"
+    )
     const data = await response.json()
     const startTimeDiff = new Date().getTime() - startTime
     diff = new Date().getTime() - new Date(data.datetime).getTime() - startTimeDiff
