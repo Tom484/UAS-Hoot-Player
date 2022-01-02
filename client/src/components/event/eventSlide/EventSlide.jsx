3062208bdb563fd48b2bd1f4200a2cce3e60a921
Tomas Kurka <tom589927@gmail.com>
2022-01-02 14:16:18 +0100

    Fix show cards bug

--- client/src/components/event/eventSlide/EventSlide.jsx
+++ client/src/components/event/eventSlide/EventSlide.jsx
@@ -4,13 +4,14 @@ import Lobby from "../lobby/Lobby"
 import SlideGame from "../slideGame/SlideGame"
 import OverallResults from "../overallResults/OverallResults"
 import { selectEventDataEvent, STATUS_TYPES } from "../../../redux/event/eventSelectors"
+import LoadingCardBackground from "../../components/loadingCardBackground/LoadingCardBackground"
 
 const EventSlide = ({ eventDataEvent }) => {
   const { status } = eventDataEvent
-
   if (status === STATUS_TYPES.LOBBY) return <Lobby />
   if (status === STATUS_TYPES.GAME) return <SlideGame />
   if (status === STATUS_TYPES.OVERALL_RESULTS) return <OverallResults />
+  return <LoadingCardBackground />
 }
 const mapStateToProps = state => ({
   eventDataEvent: selectEventDataEvent(state),
