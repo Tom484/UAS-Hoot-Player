42acea3134c52b2261c82a821540d680ec579d55
Tomas Kurka <tom589927@gmail.com>
2022-01-02 13:22:32 +0100

    Solve bug with currentDate

--- client/src/functions/time/getTimeDifference.js
+++ client/src/functions/time/getTimeDifference.js
@@ -3,14 +3,13 @@ export async function getTimeDifference() {
   try {
     const startTime = new Date()
     let diff
-    const response = await fetch(
-      "https://www.timeapi.io/api/Time/current/zone?timeZone=Europe/Prague"
-    )
+    const response = await fetch("https://time-api.netlify.app/.netlify/functions/api/date")
     const data = await response.json()
     const startTimeDiff = new Date().getTime() - startTime
-    diff = new Date().getTime() - new Date(data.datetime).getTime() - startTimeDiff
+    diff = new Date().getTime() - new Date(data.dateTime).getTime() - startTimeDiff
     return diff
   } catch (err) {
     console.log(err)
   }
 }
+
